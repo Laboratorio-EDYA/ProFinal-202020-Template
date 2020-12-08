3d358039b8079a64c85c3ae075eb6be586a58aa6
Juanchix <j.ortegam@uniandes.edu.co>
2020-12-07 23:10:04 -0500

    View ahi

--- App/view.py
+++ App/view.py
@@ -31,6 +31,7 @@ from App import controller
 from DISClib.ADT import stack
 import timeit
 assert config
+from time import process_time
 
 """
 La vista se encarga de la interacción con el usuario.
@@ -43,11 +44,164 @@ operación seleccionada.
 #  Variables
 # ___________________________________________________
 
+taxifile = 'taxi-trips-wrvz-psew-subset-small.csv'
 
 # ___________________________________________________
 #  Menu principal
 # ___________________________________________________
+def printMenu():
+    print('\n')
+    print('-'*80)
+    print('Bienvenido')
+    print('1- Inicializar analizador y cargar archivo CSV')
+    print('2- Reporte de infomación de compañías y taxis')
+    print('3- Sistemas de puntos y premios a taxis')
+    print('4- Consultar el mejor horario entre 2 areas comunitarias')
+    print('5- Salir')
+    print('-'*80)
+
+def menu1():
+    print('\n')
+    print('Reporte de infomación de compañías y taxis')
+    print('1- Número total de taxis en los servicios')
+    print('2- Número total de compañías con al menos un taxi inscrito')
+    print('3- Top M compañías ordenadas por cantidad de taxis afiliados')
+    print('4- Top N compañías que prestaron más servicios')
+    print('5- Volver al menú principal')
+
+def menu2():
+    print('\n')
+    print('Sistemas de puntos y premios a taxis')
+    print('1- Identificar los N taxis con más puntos en una fecha')
+    print('2- Identificar los M taxis con más puntos en un rango de fechas')
+    print('3- Volver al menú principal')
+
+# ----------------------------
+#     funciones menu 1
+# ----------------------------
+
+# def totalTaxis(analyzer):
+# def totalCompañias(analyzer):
+# def topM(analyzer, name, top):
+# def topN(analyzer, name, top): 
+
+# ----------------------------
+#     funciones menu 2
+# ----------------------------
+
+# def puntosFecha(analyzer, date, top):
+# def puntosRango(analyzer, date1, date2, top):
+# Debe existir una funcion en el model que calcule los puntos de cada pinche taksi
+# ------------------------------------------------------
+# def mejorHorario(analyzer, area1, area2):    Req. 3
+
+# def cargarDatos(analyzer):
 
 """
 Menu principal
 """
+
+def main():
+    analyzer = None
+    while True:
+        printMenu()
+        inputs = int(input('Selecciona una opción para continuar\n-> '))
+        if inputs == 1:   #Inicio y carga
+            t1_start = process_time() #tiempo inicial
+            print("\nInicializando.....")
+            #canalyzer = controller.init(tamaño, carga)
+            # cargaDatos(analyzer)
+            t1_stop = process_time() #tiempo final
+            print("Tiempo de ejecución ",t1_stop-t1_start," segundos ")
+
+        elif inputs == 2:   #Req. 1
+            if analyzer == None:
+                print('¡KELLY CARGUE EL ARCHIVO PRIMERO!')
+            else:
+                while True:
+                    menu1()
+                    opcion = int(input('Selecciona una opción para continuar\n--> '))
+                    if opcion == 1:
+                        # t1_start = process_time() #tiempo inicial
+                        # totalTaxis(analyzer)
+                        # t1_stop = process_time() #tiempo final
+                        # print("Esta función se ejecutó en ",t1_stop-t1_start," segundos ")
+                    elif opcion == 2:
+                        # t1_start = process_time() #tiempo inicial
+                        # totalCompañias(analyzer)
+                        # t1_stop = process_time() #tiempo final
+                        # print("Esta función se ejecutó en ",t1_stop-t1_start," segundos ")
+                    elif opcion == 3:
+                        # t1_start = process_time() #tiempo inicial
+                        # name = input('Digita el nombre de la compañía: ')
+                        # top = int(input('Digita el top límite: ))
+                        # topM(analyzer, name, top)
+                        # t1_stop = process_time() #tiempo final
+                        # print("Esta función se ejecutó en ",t1_stop-t1_start," segundos ")
+                    elif opcion == 4:
+                        # t1_start = process_time() #tiempo inicial
+                        # name = input('Digita el nombre de la compañía: ')
+                        # top = int(input('Digita el top límite: ))
+                        # topN(analyzer, name, top)
+                        # t1_stop = process_time() #tiempo final
+                        # print("Esta función se ejecutó en ",t1_stop-t1_start," segundos ")
+                    elif opcion == 5:
+                        break
+                    else:
+                        print('Opción invalida .....')
+
+        elif inputs == 3:   #Req. 2
+            if analyzer == None:
+                print('¡KELLY CARGUE EL ARCHIVO PRIMERO!')
+            else:
+                while True:
+                    menu2()
+                    opcion = int(input('Selecciona una opción para continuar\n--> '))
+                    if opcion == 1:
+                        # t1_start = process_time() #tiempo inicial
+                        # print('Digita la fecha de busqueda con formato YYYY-MM-DD')
+                        # year = input('Año: ')
+                        # month = input('Mes: )
+                        # day = input('Dia: ')
+                        # date = year + '-' + month + '-' + day
+                        # top = int(input('Digita el top límite: ))
+                        # puntosFecha(analyzer, date, top)
+                        # t1_stop = process_time() #tiempo final
+                        # print("Esta función se ejecutó en ",t1_stop-t1_start," segundos ")
+                    elif opcion == 2:
+                        # t1_start = process_time() #tiempo inicial
+                        # print('Digita la fecha inicial de busqueda con formato YYYY-MM-DD')
+                        # year1 = input('Año: ')
+                        # month1 = input('Mes: )
+                        # day1 = input('Dia: ')
+                        # date1 = year1 + '-' + month1 + '-' + day1
+                        # print('Digita la fecha ifinal de busqueda con formato YYYY-MM-DD')
+                        # year2 = input('Año: ')
+                        # month2 = input('Mes: )
+                        # day2 = input('Dia: ')
+                        # date2 = year2 + '-' + month2 + '-' + day2
+                        # top = int(input('Digita el top límite: ))
+                        # puntosRango(analyzer, date, top)
+                        # t1_stop = process_time() #tiempo final
+                        # print("Esta función se ejecutó en ",t1_stop-t1_start," segundos ")
+                    elif opcion == 3:
+                        break
+                    else:
+                        print('Opcion invalida .....')
+        elif inputs == 4:
+            if analyzer == None:
+                print('¡KELLY CARGUE EL ARCHIVO PRIMERO!')
+            else:
+                # t1_start = process_time() #tiempo inicial
+                # area1 = input('Digita el area comunitaria de inicio: ')
+                # area2 = input('Digita el area comunitaria final: ')
+                # mejorHorario(analyzer, area1, area2)
+                # t1_stop = process_time() #tiempo final
+                # print("Esta función se ejecutó en ",t1_stop-t1_start," segundos ")
+
+        elif inputs == 5:   #Salir
+            print('Cerrando el programa ...')
+            sys.exit(0)
+        else:
+            print('Opción incorrecta .....')
+main()
